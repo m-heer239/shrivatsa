cdf9e91eec4c2b3736ee0297e971a61e9ae86bf9
Hiral Mashru <himashru@conga.com>
2026-02-07 00:21:49 +0530

    delete unused items

--- products/kitchen-products.js
+++
@@ -1,7 +0,0 @@
-const queryString = window.location.search;
-            
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get('products');
-
-import data from './data.json' assert { type: 'json' };
-console.log(data);
